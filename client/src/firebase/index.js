8602eac63d6d49f9358af759570efe61aaa63315
Ashraf Ali <122337346+ashrafali2000@users.noreply.github.com>
2024-07-08 14:40:06 +0500

    Update index.js delete firebase config

--- client/src/firebase/index.js
+++ client/src/firebase/index.js
@@ -3,13 +3,7 @@ import 'firebase/auth';
 import 'firebase/storage';
 // import firebaseConfig from './config';
 const firebaseConfig = {
-    apiKey: "AIzaSyCoi0HxrqRTmNYTbbyZjlrTA2zV_DVFAg8",
-    authDomain: "vuejs-ecommerce-website-1.firebaseapp.com",
-    projectId: "vuejs-ecommerce-website-1",
-    storageBucket: "vuejs-ecommerce-website-1.appspot.com",
-    messagingSenderId: "693923065705",
-    appId: "1:693923065705:web:12f38daad8efac3dd257f8",
-    measurementId: "G-FGGSV61BB8"
+   
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
